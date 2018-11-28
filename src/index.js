5a060d9fd64b3e563c67e6a985677f6c49c85c8b
a.boykov <a.boykov@samsung.com>
2018-11-28 13:07:18 -0800

    App changes
    Signed-off-by: a.boykov <a.boykov@samsung.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import SamsungReactCard from './App'
 import * as serviceWorker from './serviceWorker';
 
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<SamsungReactCard />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
